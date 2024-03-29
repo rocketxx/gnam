744ec0e6badecf60c2dec6226663770614dd66cb
rocketxx <william97.ragusa@gmail.com>
2024-03-29 17:04:14 +0100

    aggiunto tag nessun indirizzo salvato

--- src/app/pages/profile-page/profile-page.component.ts
+++ src/app/pages/profile-page/profile-page.component.ts
@@ -7,18 +7,20 @@ import { CheckboxModule } from 'primeng/checkbox';
 import { FormsModule } from '@angular/forms';
 import { CommonModule } from '@angular/common';
 import { Utente } from '../../models/utente';
-import { MessageService } from 'primeng/api';
+import { Message, MessageService } from 'primeng/api';
 import { ToastModule } from 'primeng/toast';
+import { MessagesModule } from 'primeng/messages';
+import { TagModule } from 'primeng/tag';
 @Component({
   selector: 'app-profile-page',
   standalone: true,
-  imports: [ToastModule,CommonModule,FormsModule,ButtonModule,CheckboxModule,IconFieldModule,InputIconModule,InputTextModule,],
+  imports: [TagModule,MessagesModule,ToastModule,CommonModule,FormsModule,ButtonModule,CheckboxModule,IconFieldModule,InputIconModule,InputTextModule,],
   templateUrl: './profile-page.component.html',
   styleUrl: './profile-page.component.scss',
   providers: [MessageService]
 })
 export class ProfilePageComponent implements OnInit{
-
+  messages: Message[] = [];
   emailAddresses: string[] =['example1@example.com']; 
     utente_corrente: Utente = {
     nome: '',
@@ -36,8 +38,7 @@ export class ProfilePageComponent implements OnInit{
   }
   salvaIndirizzi()
   {
-    // window.alert(this.emailAddresses)
-    this.messageService.add({ severity: 'success', summary: 'Success', detail: 'Message Content' });
+    this.messageService.add({severity:'success', summary:'Service Message', detail:'Via MessageService'});
   }
   loadUserInfo()
   {
