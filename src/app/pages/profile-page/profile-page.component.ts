5b5a72eca5b0b2ec9f2d5f9539d366c7a765aaca
rocketxx <william97.ragusa@gmail.com>
2024-03-29 15:26:09 +0100

    aggiunto componente profilo

--- src/app/pages/profile-page/profile-page.component.ts
+++ src/app/pages/profile-page/profile-page.component.ts
@@ -1,12 +1,22 @@
 import { Component } from '@angular/core';
-
+import { IconFieldModule } from 'primeng/iconfield';
+import { InputIconModule } from 'primeng/inputicon';
+import { InputTextModule } from 'primeng/inputtext';
+import { ButtonModule } from 'primeng/button';
+import { CheckboxModule } from 'primeng/checkbox';
+import { FormsModule } from '@angular/forms';
+import { CommonModule } from '@angular/common';
 @Component({
   selector: 'app-profile-page',
   standalone: true,
-  imports: [],
+  imports: [CommonModule,FormsModule,ButtonModule,CheckboxModule,IconFieldModule,InputIconModule,InputTextModule,],
   templateUrl: './profile-page.component.html',
   styleUrl: './profile-page.component.scss'
 })
 export class ProfilePageComponent {
-
+  emailAddresses: string[] =['example1@example.com', 'example2@example.com', 'example3@example.com']; 
+  addEmail() {
+    // this.emailAddresses.push();
+    this.emailAddresses.push('');
+  }
 }
