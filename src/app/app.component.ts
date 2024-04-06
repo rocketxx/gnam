e1a0208ba41828778cdcf6005afb5d2f0bde7a56
rocketxx <william97.ragusa@gmail.com>
2024-04-06 18:59:36 +0200

    aggiunte configurazioni mancanti animation

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,14 +1,49 @@
-import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { CUSTOM_ELEMENTS_SCHEMA, Component, OnInit } from '@angular/core';
+import { RouterModule, RouterOutlet } from '@angular/router';
+import { MessageService, PrimeNGConfig } from 'primeng/api';
 import { MenuComponent } from './components/menu/menu.component';
-import { RestaurantListComponent } from './components/restaurant-list/restaurant-list.component';
+
 @Component({
-  selector: 'app-root',
-  standalone: true,
-  imports: [RouterOutlet,MenuComponent],
-  templateUrl: './app.component.html',
-  styleUrl: './app.component.scss'
+    selector: 'app-root',
+    standalone: true,
+    templateUrl: './app.component.html',
+    styleUrl: './app.component.scss',
+
+    imports: [
+      RouterOutlet,MenuComponent
+    ],
+    providers: [
+      MessageService,
+    ],
+    schemas: [CUSTOM_ELEMENTS_SCHEMA ]
 })
-export class AppComponent {
-  title = 'gnam';
+export class AppComponent implements OnInit {
+  title: string;
+
+  constructor(
+    private config: PrimeNGConfig,
+  ){
+    this.title = 'Gnam';
+  }
+
+  ngOnInit() {
+    this.configurePrime();
+  }
+
+  configurePrime()
+  {
+    this.config.ripple = true;
+    this.config.zIndex = {
+        modal   : 1100,  // dialog, sidebar
+        overlay : 1000,  // dropdown, overlaypanel
+        menu    : 1000,  // overlay menus
+        tooltip : 1100   // tooltip
+    };
+    this.config.setTranslation({
+      accept: 'Accept',
+      reject: 'Cancel',
+        //translations
+    });
+  }
+
 }
--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,10 +1,31 @@
-import { ApplicationConfig, importProvidersFrom } from '@angular/core';
-import { provideRouter } from '@angular/router';
+// import { ApplicationConfig, importProvidersFrom } from '@angular/core';
+// import { provideRouter } from '@angular/router';
+
+// import { routes } from './app.routes';
+// import { HttpClientModule } from '@angular/common/http';
+
+// export const appConfig: ApplicationConfig = {
+//   providers: [provideRouter(routes),importProvidersFrom(
+//     HttpClientModule)]
+// };
+import { ApplicationConfig, importProvidersFrom, isDevMode } from '@angular/core';
+import { /* RouterModule,  */provideRouter, withComponentInputBinding } from '@angular/router';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 import { routes } from './app.routes';
+import { AppComponent } from './app.component';
 import { HttpClientModule } from '@angular/common/http';
+import { BrowserModule } from '@angular/platform-browser';
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideRouter(routes),importProvidersFrom(
-    HttpClientModule)]
+  providers: [
+    AppComponent,
+    provideRouter(routes, withComponentInputBinding()),
+
+    importProvidersFrom(
+      HttpClientModule,
+      BrowserModule,
+      BrowserAnimationsModule,
+    ),
+]
 };
