02861abb002fdcc89c8f97b7eaa5267d0ed2815e
rocketxx <william97.ragusa@gmail.com>
2024-04-06 18:35:25 +0200

    aggiunta toaster

--- src/app/pages/custom-product/custom-product.component.ts
+++ src/app/pages/custom-product/custom-product.component.ts
@@ -1,12 +1,19 @@
 import { Component } from '@angular/core';
-
+import { MessageService } from 'primeng/api';
+import { ButtonModule } from 'primeng/button';
+import { ToastModule } from 'primeng/toast';
 @Component({
   selector: 'app-custom-product',
   standalone: true,
-  imports: [],
+  imports: [ToastModule,ButtonModule],
+  providers: [MessageService],
   templateUrl: './custom-product.component.html',
   styleUrl: './custom-product.component.scss'
 })
 export class CustomProductComponent {
+  constructor(private messageService: MessageService) {}
 
+  show() {
+      this.messageService.add({ severity: 'success', summary: 'Success', detail: 'Message Content' });
+  }
 }
