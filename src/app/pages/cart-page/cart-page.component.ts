d90b837a18920636834b79d515fef14bfbee236c
rocketxx <william97.ragusa@gmail.com>
2024-04-07 17:41:34 +0200

    creato carrello

--- src/app/pages/cart-page/cart-page.component.ts
+++ src/app/pages/cart-page/cart-page.component.ts
@@ -1,12 +1,46 @@
-import { Component } from '@angular/core';
-
+import { CommonModule } from '@angular/common';
+import { Component, OnInit } from '@angular/core';
+import { ButtonModule } from 'primeng/button';
 @Component({
   selector: 'app-cart-page',
   standalone: true,
-  imports: [],
+  imports: [CommonModule,ButtonModule],
   templateUrl: './cart-page.component.html',
   styleUrl: './cart-page.component.scss'
 })
-export class CartPageComponent {
+export class CartPageComponent implements OnInit {
+  cartItems : any[] = [];
+  loading: boolean = false;
+  ngOnInit(): void {
+    this.loadData();
+  }
+
+  salvaOrdine() {
+      this.loading = true;
+
+      setTimeout(() => {
+          this.loading = false
+      }, 2000);
+  }
+  loadData()
+  {
+   this.cartItems = [
+      {
+        "tipologia": "pizza",
+        "nome": "Pizza Margherita",
+        "ingredienti1": ["base margherita"],
+        "ingredienti2": ["salame"],
+      },
+      {
+        "tipologia": "panino",
+        "nome": "Pizza Margherita",
+        "ingredienti1": ["Porchetta"],
+        "ingredienti2": ["Mozzarella", "Funghi", "Patatine"],
+        "ingredienti3": ["Salsa rosa", "Maionese","Aroma verde"]
+      }
+    ]
+  
+   
+  }
 
 }
