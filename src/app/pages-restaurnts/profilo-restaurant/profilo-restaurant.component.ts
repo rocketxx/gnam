f9b6fb8f0e67f57cc858a6df6b542e57719bb76a
rocketxx <william97.ragusa@gmail.com>
2024-04-21 18:12:30 +0200

    creata profile pages restaurants

--- src/app/pages-restaurnts/profilo-restaurant/profilo-restaurant.component.ts
+++ src/app/pages-restaurnts/profilo-restaurant/profilo-restaurant.component.ts
@@ -1,12 +1,45 @@
-import { Component } from '@angular/core';
-
+import { CommonModule } from '@angular/common';
+import { Component, OnInit } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { MessageService } from 'primeng/api';
+import { ButtonModule } from 'primeng/button';
+import { CheckboxModule } from 'primeng/checkbox';
+import { IconFieldModule } from 'primeng/iconfield';
+import { InputIconModule } from 'primeng/inputicon';
+import { InputTextModule } from 'primeng/inputtext';
+import { MessagesModule } from 'primeng/messages';
+import { ToastModule } from 'primeng/toast';
+import { ToggleButtonModule } from 'primeng/togglebutton';
 @Component({
   selector: 'app-profilo-restaurant',
   standalone: true,
-  imports: [],
+  imports: [MessagesModule,ToastModule,ToggleButtonModule,CommonModule,FormsModule,ButtonModule,CheckboxModule,IconFieldModule,InputIconModule,InputTextModule],
   templateUrl: './profilo-restaurant.component.html',
   styleUrl: './profilo-restaurant.component.scss'
 })
-export class ProfiloRestaurantComponent {
+export class ProfiloRestaurantComponent implements OnInit{
+  dettagli_profilo : any = {
+    nome: '',
+    cognome: '',
+    telefono: '',
+    indirizzio : '',
+    stato : true
+  };
+  constructor(private messageService: MessageService)
+  {}
+  ngOnInit(): void {
+
+  }
+
+  Salva()
+  {
+  }
+  
+  modificaStatoAttivita()
+  {
+    var stato_attivita = this.dettagli_profilo.stato ? 'APERTA' : 'CHIUSA'
+    var stato_attivita_severity = this.dettagli_profilo.stato ? 'success' : 'error'
+    this.messageService.add({severity: stato_attivita_severity, summary:'Messaggio informativo', detail:'Stato attività: ' + stato_attivita});
+  }
 
 }
