e018f793c26a3656a56d7697e3b662613bea26f0
rocketxx <william97.ragusa@gmail.com>
2024-05-18 17:36:37 +0200

    pagina carica prodotto

--- src/app/pages-restaurnts/carica-prodotto/carica-prodotto.component.ts
+++ src/app/pages-restaurnts/carica-prodotto/carica-prodotto.component.ts
@@ -1,12 +1,69 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { MessageService } from 'primeng/api';
+import { FileUploadModule } from 'primeng/fileupload';
+import { ToastModule } from 'primeng/toast';
+import { CommonModule } from '@angular/common';
+import { ButtonModule } from 'primeng/button';
+import { FormsModule } from '@angular/forms';
+import { CheckboxModule } from 'primeng/checkbox';
+import { IconFieldModule } from 'primeng/iconfield';
+import { InputIconModule } from 'primeng/inputicon';
+import { InputTextModule } from 'primeng/inputtext';
+import { ToggleButtonModule } from 'primeng/togglebutton';
+import { DropdownModule } from 'primeng/dropdown';
 
+interface UploadEvent {
+    originalEvent: Event;
+    files: File[];
+}
 @Component({
   selector: 'app-carica-prodotto',
   standalone: true,
-  imports: [],
   templateUrl: './carica-prodotto.component.html',
-  styleUrl: './carica-prodotto.component.scss'
+  styleUrl: './carica-prodotto.component.scss',
+  imports: [FileUploadModule, ToastModule, CommonModule,DropdownModule,
+    ToggleButtonModule,FormsModule,ButtonModule,CheckboxModule,IconFieldModule,InputIconModule,InputTextModule
+  ],
+  
+  providers: [MessageService]
 })
-export class CaricaProdottoComponent {
+export class CaricaProdottoComponent implements OnInit{
+  uploadedFiles: any[] = [];
+  dettagli_prodotto : any = {
+    nome: '',
+    descrizione : '',
+    prezzo : 0,
+    stato : true,
+    tipologia : '',
+  };
 
+  tipologieProdottiList : any[] = [];
+  constructor(private messageService: MessageService) {}
+  ngOnInit(): void {
+    this.tipologieProdottiList =
+    [
+      { nome: 'New York', code: 'NY' },
+      { nome: 'Rome', code: 'RM' },
+      { nome: 'London', code: 'LDN' },
+      { nome: 'Istanbul', code: 'IST' },
+      { nome: 'Paris', code: 'PRS' }
+  ];
+  }
+
+  onUpload(event:any) {
+      for(let file of event.files) {
+          this.uploadedFiles.push(file);
+      }
+
+      this.messageService.add({severity: 'info', summary: 'File Uploaded', detail: ''});
+  }
+
+  Salva()
+  {
+
+  }
+  modificaStatoProdotto()
+  {
+
+  }
 }
