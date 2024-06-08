7a3055cea244756669d4e324d5a3bb162ad8e431
rocketxx <william97.ragusa@gmail.com>
2024-06-08 15:38:52 +0200

    url aggiunti

--- src/app/config/apiUrlConfig.ts
+++ src/app/config/apiUrlConfig.ts
@@ -1,8 +1,12 @@
+export const base_api = 'http://localhost:8080';
+export const base_api_restaurants = base_api + '/restaurants';
 export const apiConfig = {
     // ristorantiUrl: 'https://gnam-mock.free.beeceptor.com/ristoranti',
     ristorantiUrl: 'https://3c74b6c1-b883-4de7-acba-06c8e443a3d9.mock.pstmn.io/ristoranti',
     profiloUrl: 'https://3c74b6c1-b883-4de7-acba-06c8e443a3d9.mock.pstmn.io/profilo',
     dettaglio_ristorante : 'https://89f5accf-b373-4ad4-a63b-ef81be21e697.mock.pstmn.io/ristoranti/dettaglio/',
-    // altre api
+    all_restaurants : base_api_restaurants + '/all', 
   };
+
+  // https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcT-tUn7mE2S4p8_xMP_pCYtfhsfeXMOO6FdVA&s
   
