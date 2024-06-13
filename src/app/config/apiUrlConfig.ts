957e574245bb4d8fe539be6aef218e13455eca21
rocketxx <william97.ragusa@gmail.com>
2024-06-13 16:35:54 +0200

    commenti

--- src/app/config/apiUrlConfig.ts
+++ src/app/config/apiUrlConfig.ts
@@ -8,15 +8,20 @@ export const apiConfig = {
     all_restaurants : base_api_restaurants + '/all', 
   };
 
-  // https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcT-tUn7mE2S4p8_xMP_pCYtfhsfeXMOO6FdVA&s
+  // Nel caso non si dovesse caricare o non esiste
+  export const DEFAULT_IMAGE = "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcT-tUn7mE2S4p8_xMP_pCYtfhsfeXMOO6FdVA&s"
+  
+  //__________________________________________________
+  // _______________BASE URL__________________________
   const API_BASE_URL = 'http://localhost:8080'
-// const API_BASE_URL_MENU_ITEMS = 'menuitems'
+  // _________________________________________________
+  //__________________________________________________
+
+
   // stato dell'arte:
   // cambia quelle sopra in simili a queste sotto
   export const ENDPOINTS_MENU_ITEMS = {
     GET_MENU_ITEM_BY_ID: (id: any) => `${API_BASE_URL}/menuitems/${id}`,
     CREATE_MENU_ITEM: `${API_BASE_URL}/menuitems/create`,
     GET_MENU_ITEMS_BY_RESTAURANT_ID: (restaurantId: any) => `${API_BASE_URL}/menuitems/menu-by-restaurant/${restaurantId}`,
-    // CREATE_MENUITEM: `${API_BASE_URL}/menuitems`,
-    // Aggiungi altri endpoint qui
   };
