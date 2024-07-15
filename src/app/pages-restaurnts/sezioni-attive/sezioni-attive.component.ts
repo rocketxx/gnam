4c77a6e9bad51441e6b8c0b7fc549e883e052284
rocketxx <william97.ragusa@gmail.com>
2024-07-15 21:01:14 +0200

    fix modello

--- src/app/pages-restaurnts/sezioni-attive/sezioni-attive.component.ts
+++ src/app/pages-restaurnts/sezioni-attive/sezioni-attive.component.ts
@@ -4,6 +4,7 @@ import { MiniItemComponent } from '../../components/mini-item/mini-item.componen
 import { RestaurantsService } from '../../services/restaurants.service';
 import { idRestaurantMock } from '../../config/constantVariable';
 import { Restaurant } from '../../models/Restaurant.model';
+import { FilterItem } from '../../models/FilterItem.model';
 
 @Component({
   selector: 'app-sezioni-attive',
@@ -16,12 +17,50 @@ export class SezioniAttiveComponent implements OnInit{
   my_restaurant : Restaurant = new Restaurant() 
   constructor(private restaurant_service: RestaurantsService){}
   ngOnInit(): void {
-    // throw new Error('Method not implemented.');
+    // throw new Error('Method not implemented.');const pizza = new FilterItem();
+const pizza = new FilterItem();
+pizza.nome = "Pizza";
+pizza.isActive = false;
+pizza.restaurantId = "66682c5187762f5279d13fa8";
+
+const panino = new FilterItem();
+panino.nome = "Panino";
+panino.isActive = false;
+panino.restaurantId = "66682c5187762f5279d13fa8";
+
+const bevanda = new FilterItem();
+bevanda.nome = "Bevanda";
+bevanda.isActive = false;
+bevanda.restaurantId = "66682c5187762f5279d13fa8";
+
+const insalata = new FilterItem();
+insalata.nome = "Insalata";
+insalata.isActive = false;
+insalata.restaurantId = "66682c5187762f5279d13fa8";
+
+const primo = new FilterItem();
+primo.nome = "Primo";
+primo.isActive = false;
+primo.restaurantId = "66682c5187762f5279d13fa8";
+
+const secondo = new FilterItem();
+secondo.nome = "Secondo";
+secondo.isActive = false;
+secondo.restaurantId = "66682c5187762f5279d13fa8";
+    this.loadRestaurant();
   }
 
     loadRestaurant() {
       this.restaurant_service.getRestaurantById(idRestaurantMock).subscribe(response => {
         this.my_restaurant = response;
+      })
+    }
+
+    test()
+    {
+
+      this.restaurant_service.updateRestaurant(this.my_restaurant.id,this.my_restaurant).subscribe(response=>{
+        var tmp = response;
         debugger
       })
     }
